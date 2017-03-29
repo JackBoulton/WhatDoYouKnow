1f8f4ed6507770a46e3471903ad1a853895d1dbe
Jack Boulton <b021333f@student.staffs.ac.uk>
2017-03-30 00:08:52 +0100

    29092016test

--- app/src/main/java/dim/whatdoyouknow/actCreature.java
+++ app/src/main/java/dim/whatdoyouknow/actCreature.java
@@ -1,19 +1,22 @@
 package dim.whatdoyouknow;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class actCreature extends BaseActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_act_creature);
         super.onCreate(savedInstanceState);
-    }
-    public void gotoCreature2(View view){
-        Intent intCreature2 = new Intent(this,actCreature2.class);
-        startActivity(intCreature2);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        player = MediaPlayer.create(this, R.raw.creatureclip1);
+        player.start();
     }
 }
